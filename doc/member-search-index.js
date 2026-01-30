a515bd9468351fb9fdb5afc2b947291dc211f013
Guillermo <guillermogb@informatica.cifpaviles.pa>
2026-01-30 14:26:25 +0100

    Dejar de trackear /doc y respetar .gitignore

--- doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"agenda","c":"Agenda","l":"Agenda()","u":"%3Cinit%3E()"},{"p":"agenda","c":"Agenda","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
