539f5bfe6dbf2ea0309eefb1888123bafc684f10
MisterG <61794532+MisterGdev@users.noreply.github.com>
2026-02-02 10:20:47 +0100

    Inicializando el proyecto

---
+++ README.md
@@ -0,0 +1,7 @@
+# 2025_agenda
+
+## 30/01/2026
+
+Crea una aplicación que gestione una agenda de contactos. Usa todos los conceptos que hayamos visto hasta ahora para hacerlo.
+ 
+La aplicación deberá guardar datos de personas (nombre, apellidos, teléfonos, correo electrónico, etcétera). Permitir añadir personas, modificarlas, borrarlas, buscarlas e imprimir por pantalla la agenda completa o el resultado de una búsqueda. Crea una clase (por ejemplo, Cargador.java) que cargue en la agenda 10 personas en el momento en el que se ejecute la aplicación.
---
+++ src/agenda/Contacto.java
@@ -0,0 +1,91 @@
+package agenda;
+
+/** Utilizado para los teléfonos y los mails*/
+import java.util.HashMap;
+
+import depurador.Depurador;
+
+/**
+ * Implementación de un contacto
+ */
+public class Contacto {
+	
+	private class Telefono {
+		
+		private String prefijo;
+		private int numero;
+		
+		private Telefono(String prefijo, int numero) {
+			this.prefijo = prefijo;
+			this.numero = numero;
+		}
+		
+		private void setPrefijo(String prefijo) {
+			this.prefijo = prefijo;
+		}
+		
+		private String getPrefijo() {
+			return prefijo;
+		}
+		
+		private void setNumero(int numero) {
+			this.numero = numero;
+		}
+		
+		private String getNumero() {
+			return numero;
+		}
+		
+		public String toString() {
+			String prefijo = Integer.toString(this.prefijo);
+			String numero = Integer.toString(this.numero);
+			return this.prefijo + this.numero;
+		}
+		
+		
+	}
+	
+	/** Un contacto solo tiene un nombre, apellidos y dirección postal*/
+	private String nombre, apellidos, direccionPostal;
+	/** Tabla con la relación descripción-telefono */
+	private HashMap<String,String> telefonos;
+	/** Tabla con la relación descripcion-mail */
+	private HashMap<String,String> mails;
+	
+	/**
+	 * Constructor básico de un contacto. Todo contacto debe tener, al menos, un nombre.
+	 * @param nombre Nombre del contacto.
+	 */
+	public Contacto(String nombre) {
+		Depurador.trazar("Creando contacto. nombre="+nombre);
+		this.nombre=nombre;
+		telefonos=new HashMap<String, String>();
+		mails=new HashMap<String, String>();
+	}
+	
+	/**
+	 * Añade un teléfono al contacto
+	 * @param descripcion Tipo de teléfono (casa, trabajo, móvil, ...)
+	 * @param numero Número de teléfono
+	 */
+	public void addTelefono(String descripcion, Telefono telefono) {
+		//Se añade la descripción en mayúsculas para evitar confusiones del usuario.
+		telefonos.put(descripcion.toUpperCase(), new Telefono);
+	}
+	
+	/**
+	 * Devuelve una descripción en formato parecido a XML del contacto.
+	 */
+	public String toString() {
+		String resultado="<contacto>\n";
+		resultado+="  <nombre>"+nombre+"</nombre>\n";
+		resultado+="  <telefonos>\n";
+		resultado+="    "+telefonos.toString()+"\n";
+		resultado+="  <\\telefonos>\n";
+		resultado+="  <mails>\n";
+		resultado+="    "+mails+"\n";
+		resultado+="  <\\mails>\n";
+		resultado+="</contacto>\n";
+		return resultado;
+	}
+}
